f7c8a621d7b0be0d2e72a1ddc20322f5bffb343c
jimenalanda1 <jimenalanda0@gmail.com>
2025-03-10 21:19:14 -0400

    Fixed Puppeteer for Render

--- scraper.js
+++ scraper.js
@@ -82,9 +82,9 @@ async function fetchCheapestFlights(travelMonths, departureDay, returnDay, numRe
                 console.log(`✈️ Searching flights from EWR to ${airportToCity[destination]} (${destination}) on ${datePair.departure} → ${datePair.return}...`);
                 const browser = await puppeteer.launch({
                     headless: true,
-                    executablePath: "/usr/bin/google-chrome-stable",
                     args: ["--no-sandbox", "--disable-setuid-sandbox"]
                 });
+                
                 
                 
                 const page = await browser.newPage();
